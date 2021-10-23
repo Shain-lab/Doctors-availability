b7aaa58d17cfe22de82267eb2a76f1fb9a0f16b6
DevShain <shainaktar40@gmail.com>
2021-10-23 10:03:31 +0530

    Fetch details from randomuser generator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users ,setUsers]=useState([]);
+  useEffect(()=>{
+    (async()=>{
+      let userData;
+      try{
+        const response = await fetch('https://randomuser.me/api/?results=10');
+        const userData = await response.json().results;
+      }catch(err){
+        console.log(err);
+        userData = [];
+      }
+      setUsers(userData);
+    })();
+  }, []);
   return (
     <div className="App">
-      
+      {users.map((user,index)=>(
+        <SocialCard userData={user} key={index} />  
+      ))}
     </div>
   );
 }
